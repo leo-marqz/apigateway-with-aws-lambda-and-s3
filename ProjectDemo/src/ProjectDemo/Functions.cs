bcf25345a9d52e548bb859f41f997250e8134e24
Elmer Marquez <leomarqz2020@gmail.com>
2024-07-10 23:39:33 -0600

    se eliminaron las versiones lambda anteriores

--- ProjectDemo/src/ProjectDemo/Functions.cs
+++ ProjectDemo/src/ProjectDemo/Functions.cs
@@ -48,14 +48,12 @@ namespace ProjectDemo
                 };
             }catch (Exception ex)
             {
-                context.Logger.LogError(ex.Message);
-
                 return new APIGatewayProxyResponse
                 {
                     StatusCode = 500,
                     Body = JsonSerializer.Serialize(new
                     {
-                        errors = new List<string> { "Ups, Algo salio mal!" }
+                        message = "Algo salio mal, revisar solicitud!"
                     })
                 };
             }
@@ -74,9 +72,8 @@ namespace ProjectDemo
                         StatusCode = 400,
                         Body = JsonSerializer.Serialize(new
                         {
-                            errors = new List<string> { "BucketName Invalid!" }
-                        }),
-                        IsBase64Encoded = false
+                            message = "El nombre del bucket es requerido!"
+                        })
                     };
                 }
 
@@ -98,7 +95,6 @@ namespace ProjectDemo
                     {
                         objects = objectsS3
                     }),
-                    //Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
                 };
             }
             catch (Exception ex)
@@ -108,7 +104,7 @@ namespace ProjectDemo
                     StatusCode = 500,
                     Body = JsonSerializer.Serialize(new
                     {
-                        errors = new List<string>() { "Error, Algo salio mal con la solicitud de listar objectos" }
+                        message = "Algo salio mal!"
                     })
                 };
             }
@@ -128,9 +124,8 @@ namespace ProjectDemo
                         StatusCode = 400,
                         Body = JsonSerializer.Serialize(new
                         {
-                            errors = new List<string> { "BucketName Invalid!" }
-                        }),
-                        IsBase64Encoded = false
+                            message = "El nombre del bucket es requerido!"
+                        })
                     };
                 }
 
@@ -139,7 +134,10 @@ namespace ProjectDemo
                     return new APIGatewayProxyResponse
                     {
                         StatusCode = 400,
-                        Body = "Missing content-type header"
+                        Body = JsonSerializer.Serialize(new
+                        {
+                            message = "Encabezado content-type no valido!"
+                        })
                     };
                 }
 
@@ -157,10 +155,20 @@ namespace ProjectDemo
                     var hasContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition, out var contentDisposition);
                     if (hasContentDispositionHeader && !string.IsNullOrEmpty(contentDisposition!.FileName))
                     {
+                        string s3Key = "";
+                        bool flkeepName = Boolean.Parse(request.Headers["keep-original-file-name"]);
+                        string flname = request.Headers["form-file-name"];
 
-                        var fileExtension = Path.GetExtension(contentDisposition.FileName);
-                        var s3Key = $"{Guid.NewGuid()}{fileExtension}";
-                        s3Key = s3Key.Substring(0, s3Key.Length - 1);
+                        if (flkeepName && !string.IsNullOrEmpty(flname))
+                        {
+                            s3Key = flname.ToLower().Replace(" ", "-");
+                        }
+                        else
+                        {
+                            var fileExtension = Path.GetExtension(contentDisposition.FileName);
+                            s3Key = $"{Guid.NewGuid()}{fileExtension}";
+                            s3Key = s3Key.Substring(0, s3Key.Length - 1);
+                        }
 
                         var inputStream = new MemoryStream();
                         await section.Body.CopyToAsync(inputStream);
@@ -184,8 +192,9 @@ namespace ProjectDemo
                                     StatusCode = 200,
                                     Body = JsonSerializer.Serialize(new
                                     {
-                                        Key = s3Key,
-                                        Type = section.ContentType
+                                        key = s3Key,
+                                        type = section.ContentType,
+                                        message = "El archivo se cargo correctamente en S3!"
                                     })
                                 };
                             }
@@ -194,7 +203,10 @@ namespace ProjectDemo
                                 return new APIGatewayProxyResponse
                                 {
                                     StatusCode = (int)response.HttpStatusCode,
-                                    Body = "Error al subir el objeto a S3"
+                                    Body =JsonSerializer.Serialize(new
+                                    {
+                                        message = "Error al intentar subir el archivo a S3!"
+                                    })
                                 };
                             }
                         }
@@ -204,7 +216,10 @@ namespace ProjectDemo
                 return new APIGatewayProxyResponse
                 {
                     StatusCode = 400,
-                    Body = "Error en la request, no hay contenido"
+                    Body = JsonSerializer.Serialize(new
+                    {
+                        message = "Error en la solicitud!"
+                    })
                 };
 
             }
@@ -213,7 +228,10 @@ namespace ProjectDemo
                 return new APIGatewayProxyResponse
                 {
                     StatusCode = 500,
-                    Body = $"Error: {ex.Message} ||| Source: {ex.Source}"
+                    Body = JsonSerializer.Serialize(new
+                    {
+                        message = "Algo salio mal!"
+                    })
                 };
             }
         }
@@ -231,9 +249,8 @@ namespace ProjectDemo
                         StatusCode = 400,
                         Body = JsonSerializer.Serialize(new
                         {
-                            errors = new List<string> { "BucketName Invalid!" }
-                        }),
-                        IsBase64Encoded = false
+                            message = "El nombre del bucket es requerido!"
+                        })
                     };
                 }
 
@@ -268,225 +285,17 @@ namespace ProjectDemo
             }
             catch (AmazonS3Exception ex)
             {
-                var errorResponse = new
-                {
-                    Message = $"Error getting object {key} from bucket {bucket}",
-                    Details = ex.Message
-                };
-
                 return new APIGatewayProxyResponse
                 {
                     StatusCode = 500,
-                    Body = JsonSerializer.Serialize(errorResponse),
-                    Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+                    Body = JsonSerializer.Serialize(new
+                    {
+                        message = "Algo salio mal!"
+                    })
                 };
             }
         }
 
-
-        //=======================================================================================
-        //VERSION ANTIGUA
-        //=======================================================================================
-
-        //[LambdaFunction()]
-        //[HttpApi(LambdaHttpMethod.Get, "/list")]
-        //public async Task<APIGatewayProxyResponse> GetAll(ILambdaContext context)
-        //{
-        //    try
-        //    {
-        //        var request = new ListObjectsV2Request { BucketName = this.bucketName };
-        //        var response = await this.s3.ListObjectsV2Async(request);
-        //        var objectNames = response.S3Objects.Select(obj => new
-        //        {
-        //            key = obj.Key,
-        //            storage = obj.StorageClass.Value,
-        //            size = obj.Size
-        //        }).ToList();
-        //        var result = new
-        //        {
-        //            Objects = objectNames,
-        //            Bucket = this.bucketName
-        //        };
-
-        //        context.Logger.Log(JsonSerializer.Serialize(result));
-
-        //        return new APIGatewayProxyResponse
-        //        {
-        //            StatusCode = 200,
-        //            Body = JsonSerializer.Serialize(result),
-        //            Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-        //        };
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        context.Logger.LogError($"Error listing objects from bucket {this.bucketName}: {ex.Message}");
-
-        //        var errorResponse = new
-        //        {
-        //            Message = $"Error listing objects from bucket {this.bucketName}",
-        //            Details = ex.Message
-        //        };
-
-        //        return new APIGatewayProxyResponse
-        //        {
-        //            StatusCode = 500,
-        //            Body = JsonSerializer.Serialize(errorResponse),
-        //            Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-        //        };
-        //    }
-        //}
-
-        //[LambdaFunction()]
-        //[HttpApi(LambdaHttpMethod.Get, "/download/key/{keyname}")]
-        //public async Task<APIGatewayProxyResponse> Download(string keyname, ILambdaContext context)
-        //{
-        //    try
-        //    {
-        //        var request = new GetObjectRequest
-        //        {
-        //            BucketName = this.bucketName,
-        //            Key = keyname
-        //        };
-
-        //        using (var response = await this.s3.GetObjectAsync(request))
-        //        using (var responseStream = response.ResponseStream)
-        //        using (var memoryStream = new MemoryStream())
-        //        {
-        //            await responseStream.CopyToAsync(memoryStream);
-        //            var contentType = response.Headers["Content-Type"];
-        //            var base64 = Convert.ToBase64String(memoryStream.ToArray());
-
-        //            return new APIGatewayProxyResponse
-        //            {
-        //                StatusCode = 200,
-        //                Body = base64,
-        //                Headers = new Dictionary<string, string>
-        //                {
-        //                    { "Content-Type", contentType },
-        //                    { "Content-Disposition", $"attachment; filename={keyname}" },
-        //                    { "Content-Transfer-Encoding", "base64" }
-        //                },
-        //                IsBase64Encoded = true
-        //            };
-        //        };
-
-        //    }
-        //    catch (AmazonS3Exception ex)
-        //    {
-        //        context.Logger.LogError($"Error getting object {keyname} from bucket {this.bucketName}: {ex.Message}");
-
-        //        var errorResponse = new
-        //        {
-        //            Message = $"Error getting object {keyname} from bucket {this.bucketName}",
-        //            Details = ex.Message
-        //        };
-
-        //        return new APIGatewayProxyResponse
-        //        {
-        //            StatusCode = 500,
-        //            Body = JsonSerializer.Serialize(errorResponse),
-        //            Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-        //        };
-        //    }
-        //}
-
-        //[LambdaFunction()]
-        //[HttpApi(LambdaHttpMethod.Post, "/upload")]
-        //public async Task<APIGatewayProxyResponse> Upload(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
-        //{
-        //    try
-        //    {
-        //        if (!request.Headers.ContainsKey("content-type"))
-        //        {
-        //            return new APIGatewayProxyResponse
-        //            {
-        //                StatusCode = 400,
-        //                Body = "Missing content-type header"
-        //            };
-        //        }
-
-        //        var boundary = request.Headers["content-type"].Split(";")[1].TrimStart().Split("=")[1];
-        //        var contentBytes = Convert.FromBase64String(request.Body);
-        //        var memory = new MemoryStream(contentBytes, 0, contentBytes.Count());
-        //        var multipartReader = new MultipartReader(boundary, memory );
-        //        multipartReader.BodyLengthLimit = this.multipartBodyLengthLimit;
-        //        var section = await multipartReader.ReadNextSectionAsync();
-
-        //        byte[] bt = Convert.FromBase64String(request.Body);
-
-        //        if (section != null)
-        //        {
-        //            var hasContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition, out var contentDisposition);
-        //            if(hasContentDispositionHeader && !string.IsNullOrEmpty(contentDisposition!.FileName))
-        //            {
-
-        //                var fileExtension = Path.GetExtension(contentDisposition.FileName);
-        //                var s3Key = $"{Guid.NewGuid()}{fileExtension}";
-        //                s3Key = s3Key.Substring(0, s3Key.Length - 1);
-
-        //                var inputStream = new MemoryStream();
-        //                await section.Body.CopyToAsync(inputStream);
-
-        //                var uploadRequest = new PutObjectRequest
-        //                {
-        //                    BucketName = this.bucketName,
-        //                    Key = s3Key,
-        //                    InputStream = inputStream,
-        //                    ContentType = section.ContentType
-        //                };
-
-        //                //string hasContent = "no hay contenido";
-
-        //                //if(memory.Length > 0)
-        //                //{
-        //                //    hasContent = "si hay contenido";
-        //                //}
-
-        //                PutObjectResponse response = await this.s3.PutObjectAsync(uploadRequest);
-
-        //                if (response != null)
-        //                {
-        //                    if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
-        //                    {
-        //                        return new APIGatewayProxyResponse
-        //                        {
-        //                            StatusCode = 200,
-        //                            Body = JsonSerializer.Serialize(new
-        //                            {
-        //                                Key = s3Key,
-        //                                Type = section.ContentType
-        //                            })
-        //                        };
-        //                    }
-        //                    else
-        //                    {
-        //                        return new APIGatewayProxyResponse
-        //                        {
-        //                            StatusCode = (int)response.HttpStatusCode,
-        //                            Body = "Error al subir el objeto a S3"
-        //                        };
-        //                    }
-        //                }
-        //            }
-        //        }
-
-        //        return new APIGatewayProxyResponse
-        //        {
-        //            StatusCode = 400,
-        //            Body = "Error en la request, no hay contenido"
-        //        };
-
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        context.Logger.LogLine($"Error: {ex.Message}");
-        //        return new APIGatewayProxyResponse
-        //        {
-        //            StatusCode = 500,
-        //            Body = $"Error: {ex.Message} ||| Source: {ex.Source}"
-        //        };
-        //    }
-        //}
 
     }
 
