a364a97d7ea6f1474151557e0102ceb520128422
Elmer Marquez <leomarqz2020@gmail.com>
2024-07-15 00:09:32 -0600

    MPV V3

--- ProjectDemo/src/ProjectDemo/Functions.cs
+++ ProjectDemo/src/ProjectDemo/Functions.cs
@@ -1,11 +1,15 @@
+using Amazon;
 using Amazon.Lambda.Annotations;
 using Amazon.Lambda.Annotations.APIGateway;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using Amazon.S3;
 using Amazon.S3.Model;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.AspNetCore.WebUtilities;
+using System.Collections;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 
 
@@ -22,9 +26,9 @@ namespace ProjectDemo
 
         public Functions()
         {
-            this.bucketName = "leomarqz-storage";
+            this.bucketName = "tb-bucket-s3";
             this.s3 = new AmazonS3Client();
-            this.multipartBodyLengthLimit = 50 * 1024 * 1024; // 50 MB
+            this.multipartBodyLengthLimit = 100 * 1024 * 1024; // 100 MB
 
             //"Role": "arn:aws:iam::471112617336:role/AWSLambdaWithAmazonS3Role",
         }
@@ -41,10 +45,7 @@ namespace ProjectDemo
                 return new APIGatewayProxyResponse
                 {
                     StatusCode = 200,
-                    Body = JsonSerializer.Serialize(new
-                    {
-                        buckets = buckets
-                    }),
+                    Body = JsonSerializer.Serialize(buckets),
                 };
             }catch (Exception ex)
             {
@@ -77,7 +78,10 @@ namespace ProjectDemo
                     };
                 }
 
-                var request = new ListObjectsV2Request { BucketName = bucket };
+
+                var request = new ListObjectsV2Request { 
+                    BucketName = bucket
+                };
                 var response = await this.s3.ListObjectsV2Async(request);
 
                 var objectsS3 = response.S3Objects.Select(obj => new
@@ -86,15 +90,12 @@ namespace ProjectDemo
                     storage = obj.StorageClass.Value,
                     size = obj.Size,
                     bucket = obj.BucketName
-                }).ToList();
+                }).OrderByDescending((obj)=>obj.key).ToList();
 
                 return new APIGatewayProxyResponse
                 {
                     StatusCode = 200,
-                    Body = JsonSerializer.Serialize(new
-                    {
-                        objects = objectsS3
-                    }),
+                    Body = JsonSerializer.Serialize(objectsS3),
                 };
             }
             catch (Exception ex)
@@ -108,6 +109,79 @@ namespace ProjectDemo
                     })
                 };
             }
+        }
+
+        [LambdaFunction()]
+        [HttpApi(LambdaHttpMethod.Get, "/api/s3/{bucket}/gen-presigned-url/object")]
+        public async Task<APIGatewayProxyResponse> GetPreSignedUrlTypeGET(string bucket, APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(bucket) || string.IsNullOrWhiteSpace(bucket) )
+                { 
+                    return new APIGatewayProxyResponse
+                    {
+                        StatusCode = 400,
+                        Body = JsonSerializer.Serialize(new
+                        {
+                            message = "El nombre del bucket es requerido!"
+                        })
+                    };
+                }
+
+                if (!request.Headers.ContainsKey("s3-object-key"))
+                {
+                    return new APIGatewayProxyResponse
+                    {
+                        StatusCode = 400,
+                        Body = JsonSerializer.Serialize(new
+                        {
+                            message = "La key o nombre de archivo es requerido!"
+                        })
+                    };
+                }
+
+                var requestS3 = new GetPreSignedUrlRequest()
+                {
+                    BucketName = bucket,
+                    Key = request.Headers["s3-object-key"],
+                    Verb = HttpVerb.GET,
+                    Expires = DateTime.Now.AddHours(5)
+                };
+
+
+                string urlString = this.s3.GetPreSignedURL(requestS3);
+
+
+                if (string.IsNullOrEmpty(urlString))
+                {
+                    new APIGatewayProxyResponse
+                    {
+                        StatusCode = 400,
+                        Body = "Error en la solicitud!"
+                    };
+                }
+
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = 200,
+                    Body = JsonSerializer.Serialize(new
+                    {
+                        key = request.Headers["s3-object-key"],
+                        url = urlString,
+                        expire = requestS3.Expires.ToLocalTime(),
+                    })
+                };
+            }
+            catch (Exception ex)
+            {
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = 500,
+                    Body = "Ups, Algo salio mal"
+                };
+            }
+
         }
 
         [LambdaFunction()]
@@ -122,10 +196,7 @@ namespace ProjectDemo
                     return new APIGatewayProxyResponse
                     {
                         StatusCode = 400,
-                        Body = JsonSerializer.Serialize(new
-                        {
-                            message = "El nombre del bucket es requerido!"
-                        })
+                        Body = "El nombre del bucket es requerido!"
                     };
                 }
 
@@ -134,10 +205,7 @@ namespace ProjectDemo
                     return new APIGatewayProxyResponse
                     {
                         StatusCode = 400,
-                        Body = JsonSerializer.Serialize(new
-                        {
-                            message = "Encabezado content-type no valido!"
-                        })
+                        Body = "Encabezado content-type no valido!"
                     };
                 }
 
@@ -146,80 +214,76 @@ namespace ProjectDemo
                 var memory = new MemoryStream(contentBytes, 0, contentBytes.Count());
                 var multipartReader = new MultipartReader(boundary, memory);
                 multipartReader.BodyLengthLimit = this.multipartBodyLengthLimit;
+
                 var section = await multipartReader.ReadNextSectionAsync();
 
-                byte[] bt = Convert.FromBase64String(request.Body);
+                var objects = new List<dynamic>();
 
-                if (section != null)
+                while (section != null)
                 {
                     var hasContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition, out var contentDisposition);
                     if (hasContentDispositionHeader && !string.IsNullOrEmpty(contentDisposition!.FileName))
                     {
-                        string s3Key = "";
-                        bool flkeepName = Boolean.Parse(request.Headers["keep-original-file-name"]);
-                        string flname = request.Headers["form-file-name"];
-
-                        if (flkeepName && !string.IsNullOrEmpty(flname))
+                        var filename = Path.GetFileName(contentDisposition.FileName);
+                        var key = filename.ToLower().Replace(" ", "-").Replace("\"", "");
+                        
+                        using(var inputStream = new  MemoryStream())
                         {
-                            s3Key = flname.ToLower().Replace(" ", "-");
-                        }
-                        else
-                        {
-                            var fileExtension = Path.GetExtension(contentDisposition.FileName);
-                            s3Key = $"{Guid.NewGuid()}{fileExtension}";
-                            s3Key = s3Key.Substring(0, s3Key.Length - 1);
-                        }
+                            await section.Body.CopyToAsync(inputStream);
+                            inputStream.Position = 0;
 
-                        var inputStream = new MemoryStream();
-                        await section.Body.CopyToAsync(inputStream);
-
-                        var uploadRequest = new PutObjectRequest
-                        {
-                            BucketName = bucket,
-                            Key = s3Key,
-                            InputStream = inputStream,
-                            ContentType = section.ContentType
-                        };
-
-                        PutObjectResponse response = await this.s3.PutObjectAsync(uploadRequest);
-
-                        if (response != null)
-                        {
-                            if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                            var uploadRequest = new PutObjectRequest
                             {
-                                return new APIGatewayProxyResponse
+                                BucketName = bucket,
+                                Key = key,
+                                InputStream = inputStream,
+                                ContentType = section.ContentType
+                            };
+
+                            PutObjectResponse response = await this.s3.PutObjectAsync(uploadRequest);
+
+                            if(response != null)
+                            {
+                                if(response.HttpStatusCode == System.Net.HttpStatusCode.OK)
                                 {
-                                    StatusCode = 200,
-                                    Body = JsonSerializer.Serialize(new
+                                    objects.Add(new
                                     {
-                                        key = s3Key,
-                                        type = section.ContentType,
-                                        message = "El archivo se cargo correctamente en S3!"
-                                    })
-                                };
+                                        filename = filename,
+                                        key = key,
+                                        statusCode = (int)response.HttpStatusCode
+                                    });
+                                }
+                                else
+                                {
+                                    objects.Add(new
+                                    {
+                                        filename = filename,
+                                        key = key,
+                                        statusCode = (int)response.HttpStatusCode
+                                    });
+                                }
                             }
                             else
                             {
-                                return new APIGatewayProxyResponse
+                                objects.Add(new
                                 {
-                                    StatusCode = (int)response.HttpStatusCode,
-                                    Body =JsonSerializer.Serialize(new
-                                    {
-                                        message = "Error al intentar subir el archivo a S3!"
-                                    })
-                                };
+                                    filename = filename,
+                                    key = key,
+                                    statusCode = 500
+                                });
                             }
-                        }
-                    }
-                }
+
+                        } //end memory stream
+
+                    } //end if with hasContentDispositionHeader 
+
+                    section = await multipartReader.ReadNextSectionAsync();
+                } //end while
 
                 return new APIGatewayProxyResponse
                 {
-                    StatusCode = 400,
-                    Body = JsonSerializer.Serialize(new
-                    {
-                        message = "Error en la solicitud!"
-                    })
+                    StatusCode = 200,
+                    Body = JsonSerializer.Serialize(objects)
                 };
 
             }
@@ -237,12 +301,12 @@ namespace ProjectDemo
         }
 
         [LambdaFunction()]
-        [HttpApi(LambdaHttpMethod.Get, "/api/s3/{bucket}/download-object/{key}")]
-        public async Task<APIGatewayProxyResponse> DownloadObjectFromBucket(string bucket, string key, ILambdaContext context)
+        [HttpApi(LambdaHttpMethod.Get, "/api/s3/{bucket}/download-object")]
+        public async Task<APIGatewayProxyResponse> DownloadObjectFromBucket(string bucket, APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
         {
             try
             {
-                if (string.IsNullOrEmpty(bucket) || string.IsNullOrWhiteSpace(bucket) || string.IsNullOrEmpty(key) || string.IsNullOrWhiteSpace(key))
+                if (string.IsNullOrEmpty(bucket) || string.IsNullOrWhiteSpace(bucket) )
                 {
                     return new APIGatewayProxyResponse
                     {
@@ -254,13 +318,27 @@ namespace ProjectDemo
                     };
                 }
 
-                var request = new GetObjectRequest
+                if (!request.Headers.ContainsKey("s3-object-key"))
+                {
+                    return new APIGatewayProxyResponse
+                    {
+                        StatusCode = 400,
+                        Body = JsonSerializer.Serialize(new
+                        {
+                            message = "La key o nombre de archivo es requerido!"
+                        })
+                    };
+                }
+
+                var requestS3 = new GetObjectRequest
                 {
                     BucketName = bucket,
-                    Key = key
+                    Key = request.Headers["s3-object-key"]
                 };
 
-                using (var response = await this.s3.GetObjectAsync(request))
+                var filename = request.Headers["s3-object-key"].Split("/").Last();
+
+                using (var response = await this.s3.GetObjectAsync(requestS3))
                 using (var responseStream = response.ResponseStream)
                 using (var memoryStream = new MemoryStream())
                 {
@@ -275,7 +353,7 @@ namespace ProjectDemo
                         Headers = new Dictionary<string, string>
                         {
                             { "Content-Type", contentType },
-                            { "Content-Disposition", $"attachment; filename={key}" },
+                            { "Content-Disposition", $"attachment; filename={filename}" },
                             { "Content-Transfer-Encoding", "base64" }
                         },
                         IsBase64Encoded = true
@@ -288,10 +366,7 @@ namespace ProjectDemo
                 return new APIGatewayProxyResponse
                 {
                     StatusCode = 500,
-                    Body = JsonSerializer.Serialize(new
-                    {
-                        message = "Algo salio mal!"
-                    })
+                    Body = "Algo salio mal!"
                 };
             }
         }
